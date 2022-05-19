6fcd7dc84ac1a6e2cab5a0b1e8fe463aa3240f15
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-05-19 19:18:19 +0200

    retrieve control input and make car

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 const canvas = document.getElementById('myCanvas')
 canvas.height = window.innerHeight
-canvas.width = 200
+canvas.width = 200 
+
+//get canvas context
+const ctx = canvas.getContext('2d')
+//make car
+const car = new Car(100,100,30,50);
+car.draw(ctx)
